0b9c8871e659ff8dbd337865aeda686842c5d757
Muhammad Shayaan Ali <shayaan@Muhammads-MacBook-Air.local>
2026-05-01 05:59:36 +0530

    changed admin dash,permision of me mbers to change tak status

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_task, dashboard
+from .views import create_task, dashboard, update_task
 
 urlpatterns = [
     path('tasks', create_task),
+    path('tasks/<int:pk>', update_task),
     path('dashboard', dashboard),
 ]
